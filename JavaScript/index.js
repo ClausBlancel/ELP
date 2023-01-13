dc5b4749920711d33643d179663ae7c5cfd5a2b2
Lucas <lucas.leblanc@orange.fr>
2023-01-13 15:52:05 +0100

    Ajout detect OS + cmds lp et bing

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,22 +1,75 @@
 // Importe le module readline
 const readline = require('readline')
+const { exec } = require('child_process')
+const process = require('process')
 
-// Crée une interface
-const rl = readline.createInterface({input: process.stdin, output: process.stdout})
+CMDS = {}
+OS = process.platform
 
-// Lance le premier prompt
-rl.prompt()
-
-// Crée un évènement 'line' qui s'exécute en cas de retour chariot
-rl.on('line', (line) => {
-    if (line == "exit") {
-        process.exit()
-    } else 
-    if (line != "") {
-        console.log(`Coucou ${line} !`) 
+if (OS == "win32") {
+    CMDS = {
+        lp : "tasklist",
+        bing : {
+            cmd : "taskkill",
+            "-k" : "/f /pid",
+            "-p" : "/s /pid",
+            "-c" : "/r /pid"
+        }
     }
-    rl.prompt()
-})
+} else if (OS == "linux") {
+    CMDS = {
+        lp : "ps -aux",
+        bing : {
+            cmd : "kill",
+            "-k" : "-KILL",
+            "-p" : "-STOP",
+            "-c" : "-CONT"
+        }
+    }
+}
 
-// Ferme le prompt en cas de fermeture (Ctrl-D)
-rl.on('close', () => process.exit())
+async function listProcesses() {
+    exec(CMDS.lp, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(stdout);
+    });
+}
+
+async function killProcess(param, pid) {
+    exec(CMDS.bing.cmd + " " + CMDS.bing[param] + " " + pid, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(stdout);
+    });
+}
+
+async function main() {
+    // Crée une interface
+    const rl = readline.createInterface({input: process.stdin, output: process.stdout})
+
+    // Lance le premier prompt
+    rl.prompt()
+
+    // Crée un évènement 'line' qui s'exécute en cas de retour chariot
+    rl.on('line', async (line) => {
+        if (line == "exit") {
+            process.exit()
+        } else if (line == "lp") {
+            await listProcesses()
+        } else if (line.startsWith("bing")) {
+            await killProcess(line.split(" ")[1], line.split(" ")[2])
+        }
+        rl.prompt()
+    })
+
+    // Ferme le prompt en cas de fermeture (Ctrl-D / Ctrl-C)
+    rl.on('close', () => process.exit())
+
+}
+
+main()
