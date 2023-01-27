5a741429105669d66c3d9f9b996711a7800285b8
Lucas <lucas.leblanc@orange.fr>
2023-01-27 16:08:42 +0100

    Requête http texte

--- ELM/src/Main.elm
+++ ELM/src/Main.elm
@@ -1,9 +1,11 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, Attribute, div, input, text)
+import Html exposing (Html, Attribute, div, input, text, button)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
+import Json.Decode exposing (Decoder, field, list)
+import Http
 
 
 -- MAIN
@@ -19,33 +21,61 @@ main =
 
 -- MODEL
 
-type alias Model = { content : String }
+type alias Model = 
+    { wordToFind : String
+    , checkboxState : Bool
+    , definition : String }
 
 init : () -> ( Model, Cmd Msg )
-init content =
-    ({ content = "test" }
-    , Cmd.none)
+init model =
+    ( { wordToFind = "" 
+    , checkboxState = False 
+    , definition = ""}
+    , getDefinition "hello")
+
 
 
 -- UPDATE
 
-type Msg
-    = Change String
+type Msg = 
+    Check
+    | Change String
+    | GotText (Result Http.Error String)
+
+getDefinition : String -> Cmd Msg
+getDefinition word = 
+    Http.get {
+        url = ("https://api.dictionaryapi.dev/api/v2/entries/en/" ++ word)
+        , expect = Http.expectString GotText
+    }
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
     case msg of
-        Change newContent ->
-            ({ model | content = newContent }
-            , Cmd.none)
+        Check ->
+            ( { model | checkboxState = not model.checkboxState }, Cmd.none )
+
+        Change wordToFind ->
+            ( { model | wordToFind = wordToFind }, Cmd.none)
+
+        GotText (Ok definition) ->
+            ( { model | definition = definition }, Cmd.none)
+
+        GotText (Err err) ->
+            ( model, Cmd.none )
+
 
 
 -- VIEW
+
 view : Model -> Html Msg
 view model =
     div []
-        [ input [ placeholder "Text to replicate", value model.content, onInput Change ] []
-    , div [] [ text (model.content) ]
+    [ div [] [ text (if model.checkboxState then "hello" else "Mot mystère") ]
+    , div [] [ text (if model.wordToFind == "hello" then "Bien joué" else "Essaye de trouver") ]
+    , input [ type_ "text", onInput Change ] []
+    , input [ type_ "checkbox", onClick Check ] []
+    , div [] [ text model.definition ]
     ]
 
 -- SUBSCRIPTIONS    
